b29814e509d4b50d529ae2daae0ee7803df884cc
Mantas <sirexas@gmail.com>
2015-08-05 10:53:38 +0300

    Fix failing IndexPageTests test

--- misago/tests/apps/test_index.py
+++ misago/tests/apps/test_index.py
@@ -1,8 +1,11 @@
+from django.core.management import call_command
+
 from django_webtest import WebTest
 
 
 class IndexPageTests(WebTest):
 
     def test_index(self):
+        call_command('syncfixtures', quiet=1)
         resp = self.app.get('/')
         self.assertEqual(resp.status_int, 200)
